7322db872c2a3c43bc04eee04716110485d9d579
marcus bianchi <marcusbianchi92@hotmail.com>
2021-07-31 09:04:57 -0300

    Commit 05 Add > add-migration creationSqlite Add > update-database

---
+++ CarRentalWebProjectV0.1/Migrations/20210731120253_creationSqlite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRentalWebProjectV0._1.Migrations
+{
+    public partial class creationSqlite : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarRentals",
+                columns: table => new
+                {
+                    CarRentalId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    CpfCnpj = table.Column<int>(type: "INTEGER", nullable: false),
+                    Telephone = table.Column<int>(type: "INTEGER", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    Country = table.Column<string>(type: "TEXT", nullable: true),
+                    Model = table.Column<string>(type: "TEXT", nullable: true),
+                    LicensePlate = table.Column<string>(type: "TEXT", nullable: true),
+                    Payment = table.Column<string>(type: "TEXT", nullable: true),
+                    Withdraw = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Devolution = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarRentals", x => x.CarRentalId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarRentals");
+        }
+    }
+}
--- CarRentalWebProjectV0.1/Models/Context.cs
+++ CarRentalWebProjectV0.1/Models/Context.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+
 namespace Models {
     public class Context : DbContext {
         public DbSet<CarRental> CarRentals { get; set; }
