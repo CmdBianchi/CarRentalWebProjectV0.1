9faf4e28953c11ac50da23e28a4bed637a131584
marcus bianchi <marcusbianchi92@hotmail.com>
2021-07-31 08:42:07 -0300

    Commit 03 Add - Context

--- CarRentalWebProjectV0.1/Models/Context.cs
+++ CarRentalWebProjectV0.1/Models/Context.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 namespace Models {
-    public class Context {
+    public class Context : DbContext {
+        public DbSet<CarRental> CarRentals { get; set; }
 
+        public Context(DbContextOptions<Context> options) : base(options) {
+
+        }
     }
 }
